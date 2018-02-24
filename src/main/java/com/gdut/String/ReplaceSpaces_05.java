931c2593840f070c9fb554e1fd5725a03e96bd15
xujulong <574408578@qq.com>
2018-02-24 18:23:24 +0800

    commit with 05

---
+++ src/main/java/com/gdut/String/ReplaceSpaces_05.java
@@ -0,0 +1,61 @@
+package com.gdut.String;
+
+/**
+ * @Author: XuJuLong<5 7 4 4 0 8 5 7 8 @ qq.com>
+ * @Description:替换空格
+ * @Date: Created in 17:03 2018/2/24
+ * @
+ */
+public class ReplaceSpaces_05 {
+
+	public static int count(String string){
+		int count = 0;
+		for (int i = 0; i < string.length(); ++i) {
+			if(String.valueOf(string.charAt(i)).equals(" ")){
+				++count;
+			}
+		}
+		return count;
+	}
+
+	public static void printChar(char[] charTemp){
+		for (char i : charTemp) {
+			System.out.print(i);
+		}
+	}
+	public static void replace(String string){
+		if(string == null || string.length() <= 0){
+			return;
+		}
+		int length = string.length();
+		int newLength = length + count(string)*2;
+		char[] newTemp = new char [newLength];
+		System.arraycopy(string.toCharArray(),0,newTemp,0,string.length());
+		System.out.print("原字符：");
+		printChar(newTemp);
+		int indexOfOriginal = length-1;
+		int indexOfNew = newLength-1;
+		while(indexOfOriginal >=0 && indexOfOriginal != indexOfNew) {
+			if (newTemp[indexOfOriginal] == ' ') {
+				newTemp[indexOfNew--] = '0';
+				newTemp[indexOfNew--] = '2';
+				newTemp[indexOfNew--] = '%';
+			}else {
+				newTemp[indexOfNew--] = newTemp[indexOfOriginal];
+			}
+			--indexOfOriginal;
+		}
+		System.out.print("替换空格后的字符串：");
+		printChar(newTemp);
+	}
+
+
+//	public static void replace(String oldString){
+//		String newString = oldString.replace(" ","%20");
+//		System.out.println(newString);
+//	}
+	public static void main(String[] args) {
+		replace("We are happy.");
+	}
+
+}
