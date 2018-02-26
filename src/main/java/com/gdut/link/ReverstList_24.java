54973ef20bb0165468911dff7e981c29dc365d7b
xujulong <574408578@qq.com>
2018-02-26 20:49:06 +0800

    commit with 24

---
+++ src/main/java/com/gdut/link/ReverstList_24.java
@@ -0,0 +1,36 @@
+package com.gdut.link;
+
+/**
+ * @Author: XuJuLong<5 7 4 4 0 8 5 7 8 @ qq.com>
+ * @Description:反转链表
+ * @Date: Created in 20:47 2018/2/26
+ * @
+ */
+public class ReverstList_24 {
+
+	public ListNode ReverseList(ListNode head) {
+		ListNode pReverseNode = null;
+		ListNode preNode = null;
+		ListNode pNode = head;
+		while(pNode != null){
+			ListNode pNext = pNode.next;
+			if(pNext == null){
+				pReverseNode = pNode;
+			}
+			pNode.next = preNode;
+			preNode = pNode;
+			pNode = pNext;
+		}
+		return pReverseNode;
+	}
+
+	class ListNode {
+		int val;
+		ListNode next = null;
+
+		ListNode(int val) {
+			this.val = val;
+		}
+	}
+
+}
