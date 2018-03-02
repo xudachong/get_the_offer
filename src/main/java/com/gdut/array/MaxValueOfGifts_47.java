f63a8092d3d83433770b9ebb40135b082e4edbf3
xujulong <574408578@qq.com>
2018-03-02 17:32:46 +0800

    commit with 47

--- src/main/java/com/gdut/array/MaxValueOfGifts_47.java
+++ src/main/java/com/gdut/array/MaxValueOfGifts_47.java
@@ -29,11 +29,32 @@ public class MaxValueOfGifts_47 {
 		return max[values[0].length-1];
 	}
 
+	public static int getMaxValue_solution(int[][] values){
+		if(values == null || values.length == 0){
+			throw new IllegalArgumentException("输入有误");
+		}
+		int[][] max = new int[values.length][values[0].length];
+		for(int i = 0;i < values.length; ++i){
+			for(int j = 0;j < values[0].length;++j){
+				int left = 0;
+				int up = 0;
+				if(i > 0){
+					up = max[i-1][j];
+				}
+				if(j > 0) {
+					left = max[i][j-1];
+				}
+				max[i][j] = (up > left ? up :left) + values[i][j];
+			}
+		}
+		return max[values.length-1][values[0].length-1];
+	}
+
 	public static void main(String[] args) {
 		int[][] gifts = {{1, 10, 3, 8},
 						{12, 2, 9, 6},
 						{5, 7, 4, 11},
 						{3, 7, 16, 5}};
-		System.out.println(getMaxValue(gifts));
+		System.out.println(getMaxValue_solution(gifts));
 	}
 }
